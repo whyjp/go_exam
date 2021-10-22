8c66d8b9dd94d6eef42dc146fa5d66332909dd5a
youngjoo.park <whyj.park@gmail.com>
2021-10-22 21:01:49 +0900

    [gin-tuto] update_ logfile by gin

--- gin-tuto/server/server.go
+++ gin-tuto/server/server.go
@@ -1,11 +1,18 @@
 package server
 
 import (
+	"io"
+	"os"
+
+	"github.com/gin-gonic/gin"
 	"webzen.com/notifyhandler/config"
 )
 
 func Init() {
 	config := config.GetConfig()
 	r := NewRouter()
+	logName := config.GetString("server.log")
+	fileLog, _ := os.Create(logName)
+	gin.DefaultWriter = io.MultiWriter(fileLog)
 	r.Run(config.GetString("server.port"))
 }
