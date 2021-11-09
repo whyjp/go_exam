3122b82f001e506aeadb13c6f67fc349fda1a36c
WEBZEN\youngjoopark <youngjoopark@webzen.com>
2021-11-09 20:22:22 +0900

    [gin-tuto] notifysender 수정

--- gin-tuto/server/server.go
+++ gin-tuto/server/server.go
@@ -2,10 +2,12 @@ package server
 
 import (
 	"webzen.com/notifyhandler/config"
+	"webzen.com/notifyhandler/control/notifysender"
 )
 
 func Init() {
 	config := config.GetConfig()
+	notifysender.SetConfig(config)
 	r := NewRouter(config)
 	r.Run(config.GetString("server.port"))
 }
