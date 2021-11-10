f2f4d8cee93bd49f973bf21ad94d3c3e4223908a
WEBZEN\youngjoopark <youngjoopark@webzen.com>
2021-11-10 14:50:06 +0900

    [gin-tuto] revert error

--- gin-tuto/control/notifysender/notifysender.go
+++ gin-tuto/control/notifysender/notifysender.go
@@ -7,13 +7,10 @@ import (
 	"time"
 
 	"github.com/go-resty/resty/v2"
-	"github.com/spf13/viper"
 	"webzen.com/notifyhandler/model"
 )
 
-var defaultBaseURL = "http://10.105.33.38"
-var mailPath = "/alert/api/v2/mail"
-var teamsPath = "/alert/api/v2/teams"
+const defaultBaseURL = "http://10.105.33.38"
 
 var client *resty.Client
 
@@ -40,17 +37,26 @@ func init() {
 	// Setting global error struct that maps to Form3's error response
 	client.SetError(&Error{})
 }
-func SetConfig(config *viper.Viper) {
-	defaultBaseURL = config.GetString("notifyserver.baseuri")
-	mailPath = config.GetString("notifyserver.mail")
-	teamsPath = config.GetString("notifyserver.teams")
-}
 
 func getAPIError(resp *resty.Response) error {
 	apiError := resp.Error().(*Error)
 	return fmt.Errorf("request failed [%s]: %s", apiError.Code, apiError.Message)
 }
-func printResponse(err error, resp *resty.Response) {
+
+func SendTeams(jsonTeams *model.StNotifyTeams) (*resty.Response, error) {
+	resp, err := client.R().
+		SetBody(jsonTeams).
+		SetResult(authSuccess{}). // or SetResult(AuthSuccess{}).
+		SetError(&authError{}).   // or SetError(AuthError{}).
+		Post("/alert/api/v2/teams")
+
+	if err != nil {
+		return nil, fmt.Errorf("send team to notify server failed: %s", err)
+	}
+	// if resp.Error() != nil {
+	// 	return nil, getAPIError(resp)
+	// }
+
 	// Explore response object
 	log.Println("Response Info:")
 	log.Println("  Error      :", err)
@@ -75,38 +81,49 @@ func printResponse(err error, resp *resty.Response) {
 	log.Println("  ConnIdleTime  :", ti.ConnIdleTime)
 	log.Println("  RequestAttempt:", ti.RequestAttempt)
 	//log.Println("  RemoteAddr    :", ti.RemoteAddr.String())
-}
-func SendTeams(jsonTeams *model.StNotifyTeams) (*resty.Response, error) {
-	resp, err := client.R().
-		SetBody(jsonTeams).
-		SetResult(authSuccess{}).
-		SetError(&authError{}).
-		Post(teamsPath)
 
-	if err != nil {
-		return nil, fmt.Errorf("send teams to notify server failed: %s", err)
-	}
-	if resp.Error() != nil {
-		return nil, getAPIError(resp)
-	}
-	printResponse(err, resp)
 	return resp, nil
 }
 
 func SendMail(jsonMail *model.StNotifyMail) (*resty.Response, error) {
 	resp, err := client.R().
 		SetBody(jsonMail).
-		SetResult(authSuccess{}).
-		SetError(&authError{}).
-		Post(mailPath)
+		SetResult(authSuccess{}). // or SetResult(AuthSuccess{}).
+		SetError(&authError{}).   // or SetError(AuthError{}).
+		Post("/alert/api/v2/email")
 
 	if err != nil {
 		return nil, fmt.Errorf("send mail to notify server failed: %s", err)
 	}
-	if resp.Error() != nil {
-		return nil, getAPIError(resp)
-	}
+	// if resp.Error() != nil {
+	// 	return nil, getAPIError(resp)
+	// }
+	log.Println("request ends")
 
-	printResponse(err, resp)
+	// Explore response object
+	log.Println("Response Info:")
+	log.Println("  Error      :", err)
+	log.Println("  Status Code:", resp.StatusCode())
+	log.Println("  Status     :", resp.Status())
+	log.Println("  Proto      :", resp.Proto())
+	log.Println("  Time       :", resp.Time())
+	log.Println("  Received At:", resp.ReceivedAt())
+	log.Println("  Body       :\n", resp)
+	log.Println()
+	log.Println("Request Trace Info:")
+	ti := resp.Request.TraceInfo()
+	log.Println("  DNSLookup     :", ti.DNSLookup)
+	log.Println("  ConnTime      :", ti.ConnTime)
+	log.Println("  TCPConnTime   :", ti.TCPConnTime)
+	log.Println("  TLSHandshake  :", ti.TLSHandshake)
+	log.Println("  ServerTime    :", ti.ServerTime)
+	log.Println("  ResponseTime  :", ti.ResponseTime)
+	log.Println("  TotalTime     :", ti.TotalTime)
+	log.Println("  IsConnReused  :", ti.IsConnReused)
+	log.Println("  IsConnWasIdle :", ti.IsConnWasIdle)
+	log.Println("  ConnIdleTime  :", ti.ConnIdleTime)
+	log.Println("  RequestAttempt:", ti.RequestAttempt)
+	//log.Println("  RemoteAddr    :", ti.RemoteAddr.String())
+
 	return resp, nil
 }
--- gin-tuto/server/server.go
+++ gin-tuto/server/server.go
@@ -2,12 +2,10 @@ package server
 
 import (
 	"webzen.com/notifyhandler/config"
-	"webzen.com/notifyhandler/control/notifysender"
 )
 
 func Init() {
 	config := config.GetConfig()
-	notifysender.SetConfig(config)
 	r := NewRouter(config)
 	r.Run(config.GetString("server.port"))
 }
